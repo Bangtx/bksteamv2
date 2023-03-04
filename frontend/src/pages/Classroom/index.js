c63726d4c726d800c6327bfa918c1e6a336bae14
bangtx <bangtx@otani-trading.com>
2023-03-04 08:48:53 +0700

    fix date

--- frontend/src/pages/Classroom/index.js
+++ frontend/src/pages/Classroom/index.js
@@ -1,6 +1,4 @@
-const curr = new Date; // get current date
-const first = curr.getDate() - curr.getDay() - 1 // First day is the day of the month - the day of the week
-const last = first + 6; // last day is the first day + 6
- 
-export const firstday = new Date(curr.setDate(first)).toUTCString()
-export const lastday = new Date(curr.setDate(last)).toUTCString()
+import moment from "moment/moment";
+export const firstday = moment(new Date()).day(-30).format('YYYY-MM-DD');
+console.log(firstday)
+export const lastday = moment(new Date()).format('YYYY-MM-DD');
